c83b06308a7201d7eb269b8830b7b42da7d0960f
julianwaimann <jwaimann@makingsense.com>
2017-07-11 16:03:08 -0300

    Implement example net core client

---
+++ Split.io/Splitio.ExampleNetCore/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Splitio.Services.Client.Classes;
+
+namespace ExampleNetCore.Controllers
+{
+    public class DefaultController : Controller
+    {
+        SelfRefreshingClient sdk;
+
+        public DefaultController(MyAppData data)
+        {
+            sdk = data.Sdk as SelfRefreshingClient;
+        }
+
+        [HttpGet("")]
+        [HttpGet("default")]
+        public ActionResult Index(string featureName, string userKey)
+        {
+            if (string.IsNullOrEmpty(featureName))
+            {
+                // CHANGE THIS: Copy and paste the feature name from Split UI
+                featureName = "<feature name here>";
+            }
+
+            if (string.IsNullOrEmpty(userKey))
+            {
+                // Change this to represent your user or account id. (usually a dynamic value)
+                userKey = "userId-1";
+            }
+
+            var result = sdk.GetTreatment(userKey, featureName);
+
+            var content =  string.Format(@"<html>
+                                                <body>
+                                                    <p data-tid='test-value'>{0}</p>
+                                                </body>
+                                           </html>", result);
+
+            return new ContentResult()
+            {
+                Content = content,
+                ContentType = "text/html",
+            };
+
+        }
+    }
+}
---
+++ Split.io/Splitio.ExampleNetCore/MyAppData.cs
@@ -0,0 +1,31 @@
+﻿using Splitio.Services.Client.Classes;
+using Splitio.Services.Client.Interfaces;
+using System;
+
+
+namespace ExampleNetCore
+{
+
+    public class MyAppData
+    {
+        private ISplitClient sdk;
+        public ISplitClient Sdk { get { return sdk; } }
+
+
+        public MyAppData()
+        {
+            String apikey = "<your API KEY here>";
+
+            var configurations = new ConfigurationOptions();
+            configurations.FeaturesRefreshRate = 20;
+            configurations.SegmentsRefreshRate = 20;
+
+            configurations.ReadTimeout = 20000;
+            configurations.ConnectionTimeout = 20000;
+
+            var factory = new SplitFactory(apikey, configurations);
+            sdk = factory.Client();
+        }
+
+    }
+}
